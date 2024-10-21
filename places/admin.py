56b6a11146610b247e3b9f7d5827a18dd2be129d
Stepan Makarov <st.mkv03@gmail.com>
2024-10-21 15:04:40 +0300

    feat(site): Added the ability to edit the image in the place model

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Image, Place
 
-admin.site.register(Place)
 
-admin.site.register(Image)
+class ImageLine(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageLine]
