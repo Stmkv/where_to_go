8c35701152972aa5a7d83854d57b27b92e40eab6
Stepan Makarov <st.mkv03@gmail.com>
2024-10-23 11:08:37 +0300

    feat(site)views: add preferch related

--- places/views.py
+++ places/views.py
@@ -30,7 +30,7 @@ def index(request):
 
 
 def place(request, place_id):
-    place = get_object_or_404(Place, pk=place_id)
+    place = get_object_or_404(Place.objects.prefetch_related(), pk=place_id)
     about_place = {
         "title": place.title,
         "imgs": [image.image.url for image in place.images.all()],
