089daf98222def950cb0c114904132bc353742fb
Stepan Makarov <st.mkv03@gmail.com>
2024-10-23 11:31:00 +0300

    feat(script):get add extension url

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -24,21 +24,22 @@ class Command(BaseCommand):
         response = requests.get(json_url)
         response.raise_for_status()
         decoded_response = response.json()
-        try:
-            place, create = Place.objects.get_or_create(
-                title=decoded_response["title"],
-                short_description=decoded_response["description_short"],
-                long_description=decoded_response["description_long"],
-                longitude=decoded_response["coordinates"]["lng"],
-                latitude=decoded_response["coordinates"]["lat"],
-            )
-        except MultipleObjectsReturned:
-            logging.info("Такое место уже есть в базе")
+
+        place, create = Place.objects.get_or_create(
+            title=decoded_response["title"],
+            short_description=decoded_response["description_short"],
+            long_description=decoded_response["description_long"],
+            longitude=decoded_response["coordinates"]["lng"],
+            latitude=decoded_response["coordinates"]["lat"],
+        )
 
         for num, url in enumerate(decoded_response["imgs"], start=1):
+            file_extension = url.split(".")[-1]
             response = requests.get(url)
             response.raise_for_status()
-            filename = f"{num}. {decoded_response['description_short']}.jpg"
+            filename = (
+                f"{num}. {decoded_response['description_short']}.{file_extension}"
+            )
             Image.objects.create(
                 place=place,
                 image=ContentFile(response.content, filename),
