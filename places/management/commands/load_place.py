abfb51a87ed5c60088fe542af75550803d7e271d
Stepan Makarov <st.mkv03@gmail.com>
2024-10-23 11:36:53 +0300

    feat(script): add error handler

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,7 +1,7 @@
 import logging
+import time
 
 import requests
-from django.core.exceptions import MultipleObjectsReturned
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
 
@@ -21,27 +21,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         json_url = options["json_url"]
-        response = requests.get(json_url)
-        response.raise_for_status()
-        decoded_response = response.json()
-
-        place, create = Place.objects.get_or_create(
-            title=decoded_response["title"],
-            short_description=decoded_response["description_short"],
-            long_description=decoded_response["description_long"],
-            longitude=decoded_response["coordinates"]["lng"],
-            latitude=decoded_response["coordinates"]["lat"],
-        )
-
-        for num, url in enumerate(decoded_response["imgs"], start=1):
-            file_extension = url.split(".")[-1]
-            response = requests.get(url)
+        try:
+            response = requests.get(json_url)
             response.raise_for_status()
-            filename = (
-                f"{num}. {decoded_response['description_short']}.{file_extension}"
+            decoded_response = response.json()
+
+            place, create = Place.objects.get_or_create(
+                title=decoded_response["title"],
+                short_description=decoded_response["description_short"],
+                long_description=decoded_response["description_long"],
+                longitude=decoded_response["coordinates"]["lng"],
+                latitude=decoded_response["coordinates"]["lat"],
             )
-            Image.objects.create(
-                place=place,
-                image=ContentFile(response.content, filename),
-                number_image=num,
-            )
+
+            for num, url in enumerate(decoded_response["imgs"], start=1):
+                file_extension = url.split(".")[-1]
+                response = requests.get(url)
+                response.raise_for_status()
+                filename = (
+                    f"{num}. {decoded_response['description_short']}.{file_extension}"
+                )
+                Image.objects.create(
+                    place=place,
+                    image=ContentFile(response.content, filename),
+                    number_image=num,
+                )
+        except requests.exceptions.HTTPError:
+            logging.info("Ошибка подключения")
+        except requests.exceptions.ConnectionError:
+            logging.info("Ошибка соединения")
+            time.sleep(5)
